346241c6a9d7e2cd4c318a6ab672d6b6304375a2
ianchen23 <ianchen23@gmail.com>
2018-04-08 22:43:47 -0500

    Delete sample folder

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        Player player = new Player("file:///C:/Memento.2000.720p.BluRay.x264.YIFY.mp4");
+        Scene scene = new Scene(player, 1280, 528, Color.BLACK);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
