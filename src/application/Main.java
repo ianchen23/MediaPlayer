c4a7ca321f70bef81b8bc22176e6b016e90517a3
ianchen23 <ianchen23@gmail.com>
2018-04-08 23:06:50 -0500

    Innitial Push

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -11,6 +14,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage){
+
+        MenuItem open = new MenuItem("Open");
+        Menu file = new Menu("File");
+        MenuBar menu = new MenuBar();
+
+        file.getItems().add(open);
+        menu.getMenus().add(file);
+
         Player player = new Player("file:///C:/Memento.2000.720p.BluRay.x264.YIFY.mp4");
         Scene scene = new Scene(player, 1280, 528, Color.BLACK);
         primaryStage.setScene(scene);
